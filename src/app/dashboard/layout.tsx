b16fdee4932096fdbe7830434e64f2ad36974810
Mekin Jemal <mekinjemal999@gmail.com>
2025-04-23 15:39:09 +0300

    add new pages for analytics logs, API keys, billing subscription, and team management

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,30 @@
+// app/dashboard/layout.tsx
+import React, { ReactNode } from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SearchProvider } from "@/context/search-context";
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+
+export default async function DashboardLayout({ children }: { children: ReactNode }) {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  // Uncomment this to enforce authentication
+  // if (!session) {
+  //   redirect("/auth/login");
+  // }
+
+  return (
+    <SidebarProvider className="dark:bg-[#16423C] p-3 dark:pt-1 font-sora ">
+      <SearchProvider>
+        <AppSidebar />
+        <SidebarInset className="ml-3">
+          <div className="dark:bg-[#021815] rounded-xl">
+            {children}
+          </div>
+        </SidebarInset>
+      </SearchProvider>
+    </SidebarProvider>
+  );
+}
